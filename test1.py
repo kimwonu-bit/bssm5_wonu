15a3808a19c148a9553255831f6b0bbd2ffb5d9e
김원우 <25_13@bssm.hs.kr>
2025-05-07 20:00:01 +0900

    수정

--- test1.py
+++ test1.py
@@ -3,6 +3,3 @@ import streamlit as st
 st.title('this is title')
 st.header('this is header')
 st.subheader('this is subheader')
-
-git config --global user.name "김원우"
-git config --global user.email "25_13@bssm.hs.kr"
