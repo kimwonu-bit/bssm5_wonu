ea9079b5217400a05b255b818904b38d737f8311
김원우 <25_13@bssm.hs.kr>
2025-05-07 20:28:57 +0900

    추가

--- test1.py
+++ test1.py
@@ -5,7 +5,8 @@ tab1, tab2 = st.tabs(['은', '총'])
 with col1 :
     st.title("here is column1 title")
     with tab1 :
-        st.write('은총이\n...\n솔직히\n종종\n바보같을때\n있긴 해')
+        st.write('은총이...솔직히종종바보같을때있긴 해')
+        st.write('은총이...솔직히종종바보같을때있긴 해')
     with tab2 :
         st.write("은총아...\n\n\n\n\n\n\n\n\n\n\n\n\n캘빈이 뭐에요?????????")
 
